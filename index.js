bc4f93098474d6aa2bfe7e1e1d8d9810b6aa3fad
William Holmes <holmesw@ie.ibm.com>
2018-10-19 13:37:44 +0100

    Update response

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const API = require('./api');
 app.authenticate().then(() => app.uploadPhoto('./appicon.jpg'));
 
 const getCard = data => {
-    console.log('TCL: getCard data', data);
     const { name = '', description, people = [] } = data;
     const subTitle = `${people.length} contact${people.length == 1 ? '' : 's'}`;
     const actionId = `${constants.ACTION_DETAILS}${JSON.stringify({ name, description })}`;
@@ -24,7 +23,6 @@ const getCard = data => {
 const getCards = services => _.chain(services).sort('description').map(getCard).value();
 
 const postCards = (message, annotation, services) => {
-    console.log('TCL: postCards -> services', services);
     app.sendTargetedMessage(message.userId, annotation, getCards(services));
 };
 
@@ -36,15 +34,12 @@ const serviceNotFound = (serviceName, message, annotation) => {
 const getService = (message, annotation, params) => {
     const { spaceId } = message;
     const serviceName = _.first(params);
-    console.log('TCL: getService -> serviceName', serviceName, spaceId);
     API.getService(serviceName).then(services => {
-        console.log('TCL: API.getService -> services', services);
         if (services.length) {
             return postCards(message, annotation, services);
         }
         throw new Error('Service Not found');
     }).catch(err => {
-        console.log('TCL: getService -> err', err, serviceName);
         serviceNotFound(serviceName, message, annotation);
     })
 };
@@ -52,25 +47,16 @@ const getService = (message, annotation, params) => {
 const onActionSelected = (message, annotation) => {
     const { userId } = message;
     const { actionId = '' } = annotation;
-    console.log('TCL: onActionSelected -> actionId', actionId);
     if (actionId.startsWith(constants.ACTION_DETAILS)) {
         const { description } = JSON.parse(actionId.split(constants.ACTION_DETAILS)[1]);
-        console.log('TCL: onActionSelected -> description', description);
         API.getService(description).then(services => {
-            console.log('TCL: API.getService -> services', services);
             if (services.length) {
                 const { name, people } = _.first(services);
-
-                const title = `${name}`;
-                const service = `${description.split(' ').join('\n')}`
-                const contacts = _.map(people, ({ id, displayName }) => `<@${id}|${displayName}>`).join('\n');
-
-                const body = [service, contacts].join('\n\n');
-                app.sendTargetedMessage(userId, annotation, UI.generic(title, body))
+                const body = _.map(people, ({ id, displayName }) => `<@${id}|${displayName}>`).join('\n');
+                app.sendTargetedMessage(userId, annotation, UI.generic(name, body))
             }
             throw new Error('Service Not found');
         }).catch(err => {
-            console.log('TCL: getService -> err', err, serviceName);
             serviceNotFound(name, message, annotation);
         })
     }
