c302d5adcdd9848461878e12038404be5e61f956
William Holmes <holmesw@ie.ibm.com>
2018-10-28 00:23:43 +0100

    update API name

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const findCommitters = (message, annotation, params) => {
     const repository = _.first(params);
     console.log('TCL: findCommitters -> repository', repository);
     const { userId } = message;
-    API.repository(repository).then(repositories => {
+    API.getRepository(repository).then(repositories => {
         console.log('TCL: findCommitters -> repositories', repositories);
         app.sendTargetedMessage(userId, annotation, UI.generic('test', JSON.stringify(repositories), []));
     });
