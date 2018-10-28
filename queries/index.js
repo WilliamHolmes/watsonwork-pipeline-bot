fb4042cf99c159d8a11781ffd91910068d1a4604
William Holmes <holmesw@ie.ibm.com>
2018-10-28 20:12:48 +0000

    Update query

--- queries/index.js
+++ queries/index.js
@@ -1,16 +1,19 @@
 const query = `query {
   organization(login:"toscana"){
     teams(first:100){
-      nodes{
+      nodes {
+        id
         name
         members(first: 100) {
           nodes {
+            id
             email
             name
           }
         }
         repositories(first:100){
-          nodes{
+          nodes {
+            id
             name
           }
         }
